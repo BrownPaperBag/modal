0011d0a2720947ff2fe1d05af756a99c6dc97252
Oran Fry <oran@brownpaperbag.co.nz>
2014-06-12 10:58:40 +1200

    development of modal

--- directives/modal.js
+++ directives/modal.js
@@ -3,21 +3,20 @@ angular.module('modal', [])
     .directive('modal', function () {
         return {
             restrict: 'E',
-            scope: {
-                show: '=',
-            },
             transclude: true,
+            replace: true,
             templateUrl: '/bower/modal/directives/modal.html',
             link: function($scope, $element, $attributes) {
-                $('.angular-popup', $element).addClass($attributes.classes);
+                $('.angular-popup__dialog', $element).addClass($attributes.classes);
                 $scope.hide = function($event) {
                     $event.stopPropagation();
+                    var eventScope = $scope.$new();
+                    eventScope.$event = $event;
                     if (typeof $attributes.onclose !== 'undefined') {
-                        if (!$scope.$eval($attributes.onclose)) {
+                        if (!eventScope.$eval($attributes.onclose)) {
                             return;
                         }
                     }
-                    $scope.show = false;
                 }
             }
 
